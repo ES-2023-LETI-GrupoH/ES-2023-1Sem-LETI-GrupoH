9074e75d1097c15f035d5d6bb022919b194e285e
gfbas-iscte <gfbas@iscte-iul.pt>
2023-10-23 20:45:09 +0100

    Changes made so it is possible to read sentences separated by commas.

--- src/main/java/csv_loading/main.java
+++ src/main/java/csv_loading/main.java
@@ -5,15 +5,38 @@ import java.util.Scanner;
 
 public class main {
 
+            public static void main(String args[]) {
+                Scanner sc = new Scanner("João, Alberto, olá, apple, Samsung, Israel");
+                    sc.useDelimiter(delimiter);
+                        String [][] matriz = new String[5][5];
+                             for(int i=0; i<10; i++) {
+                                 for (int j = 0; j < 10; j++) {
+                                     while (sc.hasNext()) {
+                                         matriz[i][j] = sc.next();
+                                         System.out.println(matriz);
+                                     }
+                                 }
+                             }
+       //             while (sc.hasNext()) {
+       //                 String temporario = sc.next();
+       //                     System.out.println(temporario);
+
+            }
+
     private static String delimiter = ",";
 
-    public static void setDelimeter(String s){
+    public static void setDelimeter(String s) {
         delimiter = s;
     }
 
-    public static void parse(FileReader f){
-        Scanner sc = new Scanner(f);
-        sc.useDelimiter(delimiter);
-    }
+    public static void parse(FileReader f) {
+        String [][] matriz = new String[100][100];
+            Scanner sc = new Scanner(f);
+                sc.useDelimiter(delimiter);
+                    while (sc.hasNext()) {
+                        String temporario = sc.next();
+                            System.out.println(temporario);
+                    }
 
+    }
 }
