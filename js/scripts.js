111a0fb127979384714c7f108a5a9fb48ddae9fa
FilipeS01 <145039791+FilipeS01@users.noreply.github.com>
2023-10-24 12:01:25 +0100

    added comments for easier revision

--- js/scripts.js
+++ js/scripts.js
@@ -12,37 +12,50 @@ csvForm.addEventListener("submit", function (event) {
 
         reader.onload = function (e) {
             const csvContent = e.target.result;
-            // Aqui você pode processar os dados do CSV e exibi-los no elemento csvDataDisplay
+            // CSV information processing
             csvDataDisplay.textContent = "Conteúdo do arquivo CSV:\n" + csvContent;
         };
 
-        reader.readAsText(file);
+        reader.readAsText(file); //read the file name
     } else {
         csvDataDisplay.textContent = "Nenhum arquivo selecionado.";
+        //if no file is found it displays ""Nenhum arquivo selecionado.""
     }
 });
 
+// Select the HTML table element with the 'table' tag and assign it to the 'table' variable.
 const table = document.querySelector('table');
 
-for (let hour = 8; hour < 22; hour++) {
-    for (let minute = 0; minute < 60; minute += 30) {
+// Loop to create time intervals between 8:00 (8 AM) and 23:00 (11:00 PM).
+for (let hour = 8; hour < 23; hour++) { // Loop through hours from 8 to 22 (inclusive).
+    for (let minute = 0; minute < 60; minute += 30) { // Loop through minutes, incrementing by 30.
+        // Create the start and end times in the format "HH:MM" (e.g., "8:00-8:30").
         let timeStart = `${hour}:${minute < 10 ? '0' : ''}${minute}`;
+        // Determine the next hour and minute for the end time of the time interval.
         let nextHour = minute === 30 ? hour + 1 : hour;
+        // If the current minute is 30, increment the hour by 1; otherwise, keep the same hour.
         let nextMinute = minute === 30 ? 0 : minute + 30;
+        // Create the end time in the format "HH:MM" (e.g., "8:00-8:30").
         let timeEnd = `${nextHour}:${nextMinute < 10 ? '0' : ''}${nextMinute}`;
 
+        // Create a new table row for each time interval.
         const row = document.createElement('tr');
+
+        // Create a table header cell (th) for the time interval and set its class to 'tempo'.
         const timeCell = document.createElement('th');
         timeCell.className = 'tempo';
         timeCell.textContent = `${timeStart}-${timeEnd}`;
         row.appendChild(timeCell);
 
-        for (let day = 0; day < 6; day++) {
+        // Loop through the days of the week (7 days) and create a cell (td) for each day.
+        for (let day = 0; day < 7; day++) {
+            // Create a table data cell for the schedule information and set its class to 'aula'.
             const cell = document.createElement('td');
             cell.className = 'aula';
             row.appendChild(cell);
         }
 
+        // Append the row to the table, adding it to the grid of cells.
         table.appendChild(row);
     }
 }
