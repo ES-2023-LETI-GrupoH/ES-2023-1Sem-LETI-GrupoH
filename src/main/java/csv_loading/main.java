1b9e6dd022fa4d27464b54b340f966ff9722a112
vb-03 <vb2003.12@gmail.com>
2023-10-25 00:24:11 +0100

    Still with the same issue, however trying new approaches.

--- src/main/java/csv_loading/main.java
+++ src/main/java/csv_loading/main.java
@@ -2,25 +2,14 @@ package csv_loading;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class main {
 
     static String[][] matriz = new String[10][150];
-
-    public static void main(String args[]) {
-        setDelimiter(";");
-        FileReader testread;
-        try {
-            testread = new FileReader("C:/Users/vb200/IdeaProjects/ES-2023-1Sem-LETI-GrupoH/src/main/java/csv_loading/minihorario.csv");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        parse(testread);
-        print2D(matriz);
-    }
-
-    private static String delimiter = ",";
+    private static String delimiter = ";";
 
     public static void setDelimiter(String s) {
         delimiter = s;
@@ -29,11 +18,34 @@ public class main {
     public static void parse(FileReader f) {
         Scanner sc = new Scanner(f);
         sc.useDelimiter(delimiter);
-            for (int x = 0; x < matriz.length ; x++) {
-                for(int y = 0; y < matriz[x].length; y++) {
+//        int i = 0;
+//        HashMap<Integer,String> h = new HashMap<>();
+//        while (lines.hasNextLine()) {
+////            matriz[0][i] = lines.nextLine();
+////            System.out.println(matriz[0][i]);
+////            i++;
+//            h.put(i,lines.nextLine());
+//            i++;
+//        }
+//        lines.close();
+//        for(int x = 0; x < h.size(); x++){
+//            String[] s = h.get(x).split(delimiter);
+//            for(int y = 0; y < s.length; y++) {
+//                matriz[x][y] = s[y];
+//            }
+//            //matriz[x] = s;
+//            //System.out.println(Arrays.toString(s));
+//        }
+
+        for (int x = 0; x < matriz.length; x++) {
+            for (int y = 0; y < matriz[x].length; y++) {
+                //if (lines.hasNext()) {
                     matriz[x][y] = sc.next();
-                }
+               // } else {
+                //    break;
+               // }
             }
+        }
         sc.close();
     }
 
@@ -45,13 +57,26 @@ public class main {
         return b;
     }
 
-    public static void print2D(String mat[][]) {
-        // Loop through all rows
-        for (int i = 0; i < mat.length; i++)
-
-            // Loop through all elements of current row
-            for (int j = 0; j < mat[i].length; j++)
-                System.out.print(mat[i][j] + "|");
-
+    public static void print2D(String[][] mat) {
+        for (String[] row : mat) {
+            for (String element : row) {
+                System.out.print(element + "|");
+            }
+            //System.out.println();
+        }
     }
+
+
+    public static void main(String args[]) {
+        setDelimiter(";");
+        FileReader testread;
+        try {
+            testread = new FileReader("src/main/java/csv_loading/minihorario.csv");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        parse(testread);
+        print2D(matriz);
+    }
+
 }
