0af7656043f27466bde23f95a7dc9440d9a95f54
vb-03 <vb2003.12@gmail.com>
2023-11-07 19:57:08 +0000

    Remote parsing working - done with @gfbas-iscte over screen share

--- src/main/java/csv_loading/main.java
+++ src/main/java/csv_loading/main.java
@@ -1,7 +1,7 @@
 package csv_loading;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,19 +9,37 @@ import java.util.Scanner;
 public class main {
 
     private static String delimiter = ";";
+    private static String remoteInput;
 
     public static void setDelimiter(String s) {
         delimiter = s;
     }
 
-    public static void getRemoteFile(String url){
-
-    }
 
     static List<String[]> dataList = new ArrayList<>(); // Create a list to store each line of fields in the CSV
     static int maximumNumberOfFields = 0;  // Initialize a variable to track the maximum number of fields in any line
     static int totalNumberOfLines;
     static String[][] data;
+
+    public static void getandParseRemoteFile(String url) throws IOException {
+        URL remote = new URL(url);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(remote.openStream()));
+        while ((remoteInput = in.readLine()) != null){
+            String[] fields = remoteInput.split(delimiter); // Split the line into fields using the semicolon (;) as the delimiter
+            maximumNumberOfFields = Math.max(maximumNumberOfFields, fields.length); // Update the maximum number of fields if necessary
+            dataList.add(fields); // Add the fields to the list
+        }
+        in.close();
+
+        totalNumberOfLines = dataList.size(); // Calculate the total number of lines in the CSV
+        data = new String[totalNumberOfLines][maximumNumberOfFields]; // Create a two-dimensional array to store the CSV data
+        for (int i = 0; i < totalNumberOfLines; i++) {// Copy the data from the list into the two-dimensional array
+            String[] fields = dataList.get(i);
+            System.arraycopy(fields, 0, data[i], 0, fields.length); // Use System.arraycopy to copy the fields into the array
+        }
+
+    }
 
     public static void parse(FileReader texto) {
         Scanner scanner = new Scanner(texto);  // Initialize a scanner to read the CSV data
@@ -53,15 +71,11 @@ public class main {
     }
 
 
-        public static void main(String[] args){
+        public static void main(String[] args) throws IOException {
             setDelimiter(";");
-            FileReader testread;
-            try {
-                testread = new FileReader("src/main/java/csv_loading/minihorario.csv");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            parse(testread);
+            //FileReader testread;
+            //parse(testread);
+            getandParseRemoteFile("https://raw.githubusercontent.com/ES-2023-LETI-GrupoH/ES-2023-1Sem-LETI-GrupoH/obj-01/src/main/java/csv_loading/minihorario.csv?token=GHSAT0AAAAAACHEEKKTPINYI5FRVG5E4WKGZKKSZVQ");
             print();
         }
 
